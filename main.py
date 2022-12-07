a78dab606249364ae34b263b410908152ce1192c
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-12-07 10:24:05 +0200

    coding

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge()
+
+driver.get('https://www.magshimim.cyber.org.il/')
+elem = driver.find_element(By.PARTIAL_LINK_TEXT, "רוצה להירשם")
+elem.click()
+
+if driver.current_url == "https://www.magshimim.cyber.org.il/closedreg":
+    print("finished registration :-(")
+else:
+    print("registration opened :-)")
+
+driver.close()
