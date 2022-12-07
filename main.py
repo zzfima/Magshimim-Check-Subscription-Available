8923653a7afcc5cf573412b48476cb9cca0ef277
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-12-07 10:50:51 +0200

    add smtp

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import smtplib
+from email.mime.text import MIMEText
 
-driver = webdriver.Edge()
 
-driver.get('https://www.magshimim.cyber.org.il/')
-elem = driver.find_element(By.PARTIAL_LINK_TEXT, "רוצה להירשם")
-elem.click()
+def send_email_smtp(msg_text):
+    msg = MIMEText(msg_text)
+    msg['Subject'] = 'Magshimim subscription'
+    msg['From'] = 'Magshimim.subscription@gmail.com'
+    msg['To'] = 'zzfima@gmail.com'
 
-if driver.current_url == "https://www.magshimim.cyber.org.il/closedreg":
-    print("finished registration :-(")
-else:
-    print("registration opened :-)")
+    s = smtplib.SMTP('smtp.office365.com', 587)
+    s.sendmail('Magshimim.subscription@gmail.com', 'zzfima@gmail.com', msg.as_string())
+    s.quit()
 
-driver.close()
+
+if __name__ == '__main__':
+    driver = webdriver.Edge()
+
+    driver.get('https://www.magshimim.cyber.org.il/')
+    elem = driver.find_element(By.PARTIAL_LINK_TEXT, "רוצה להירשם")
+    elem.click()
+
+    if driver.current_url == "https://www.magshimim.cyber.org.il/closedreg":
+        send_email_smtp("finished registration :-(")
+    else:
+        send_email_smtp("registration opened :-)")
+
+    driver.close()
