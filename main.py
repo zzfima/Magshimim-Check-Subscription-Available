3ec47371f56ef7eb2a9b44ebdb084148d8851201
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-12-08 20:34:22 +0200

    check with msgbox

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import smtplib
-from email.mime.text import MIMEText
-
-
-def send_email_smtp(msg_text):
-    msg = MIMEText(msg_text)
-    msg['Subject'] = 'Magshimim subscription'
-    msg['From'] = 'Magshimim.subscription@gmail.com'
-    msg['To'] = 'zzfima@gmail.com'
-
-    s = smtplib.SMTP('smtp.office365.com', 587)
-    s.sendmail('Magshimim.subscription@gmail.com', 'zzfima@gmail.com', msg.as_string())
-    s.quit()
-
+import ctypes
 
 if __name__ == '__main__':
     driver = webdriver.Edge()
@@ -22,9 +9,10 @@ if __name__ == '__main__':
     elem = driver.find_element(By.PARTIAL_LINK_TEXT, "רוצה להירשם")
     elem.click()
 
-    if driver.current_url == "https://www.magshimim.cyber.org.il/closedreg":
-        send_email_smtp("finished registration :-(")
-    else:
-        send_email_smtp("registration opened :-)")
-
+    current_url = driver.current_url
     driver.close()
+
+    if current_url == "https://www.magshimim.cyber.org.il/closedreg":
+        ctypes.windll.user32.MessageBoxW(0, "finished registration :-(", "Registration", 1)
+    else:
+        ctypes.windll.user32.MessageBoxW(0, "registration opened :-)", "Registration", 1)
